f629b01be6acb88ddb6fd3a48f2013297255bfc3
Marko Kužner <marko.kuzner1@gmail.com>
2019-02-15 00:48:30 +0100

    dodana ikona, narejen splash screen, narejen prijavni screen(ne se do konca)

---
+++ app/src/main/java/si/inova/zimskasola/LogIn.kt
@@ -0,0 +1,13 @@
+package si.inova.zimskasola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zimskasola.R
+
+class LogIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
---
+++ app/src/main/java/si/inova/zimskasola/SplashScreen.kt
@@ -0,0 +1,13 @@
+package si.inova.zimskasola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zimskasola.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
